3563f58ea38a5bed795a65ee052b5c93f00c1eb3
Reynardo Etenia Wongso <reynardoew@gmail.com>
2017-06-10 21:24:08 +0700

    First commit working without carousel support

---
+++ assets/js/insta.js
@@ -0,0 +1,17 @@
+function getPic(link){	
+	var url = "http://api.instagram.com/oembed?url="+link;
+    $.ajax({
+        url: url,
+        dataType: "jsonp",
+        cache: false,
+        success: function (data) {
+			var hdpicurl = data.thumbnail_url.replace(/s[0-9\.]+/i, "");
+            var ath = "Author: " + data.author_name;
+            var cpt = "Caption:" + data.title;
+			
+			
+			var result = "<img src='" + hdpicurl + "'/> <br>"+ ath +"<br> <br>" + cpt + "<br>";
+			$(".result").append(result);		
+		}
+    });
+}
